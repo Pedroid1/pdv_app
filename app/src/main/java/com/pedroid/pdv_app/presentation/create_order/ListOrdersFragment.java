0c3a54aad6e5a5b437288f217d6212c9b274cb3a
Pedro Henrique Souza Araujo <pedro.steam2016@hotmail.com>
2025-03-12 10:28:35 -0300

    first layout - ListOrdersFragment

---
+++ app/src/main/java/com/pedroid/pdv_app/presentation/create_order/ListOrdersFragment.java
@@ -0,0 +1,31 @@
+package com.pedroid.pdv_app.presentation.create_order;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pedroid.pdv_app.R;
+import com.pedroid.pdv_app.databinding.FragmentListOrdersBinding;
+
+public class ListOrdersFragment extends Fragment {
+
+    private FragmentListOrdersBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentListOrdersBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
