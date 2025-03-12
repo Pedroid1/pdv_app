ca8dfcc9a495caec2ce28bc25917735e4b2dbf7d
Pedro Henrique Souza Araujo <pedro.steam2016@hotmail.com>
2025-03-12 10:14:45 -0300

    navigation component dependency and structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("androidx.navigation.safeargs") version "2.8.8" apply false
 }
